6a1574a3bb529e77cda89eb7c85dc6c4802c1986
Ning Gao <ning.gao@colorado.edu>
2016-08-04 14:39:11 -0600

    user specified up block support

--- vivado-build/StartBuild.py
+++ vivado-build/StartBuild.py
@@ -22,6 +22,7 @@ def runBashOutput(command):
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--vivado",dest="vivado", help="vivado version: supported are 2014.4, 2015.1", required = True)
 parser.add_argument("-c", "--config", dest="config", help="config uuid in redis db", required = True)
+parser.add_argument("-i", "--iplib", dest="iplib", help="ip block library", required = False)
 args = parser.parse_args()
 
 config_json = RedisHelper.GetConfig(args.config) 
@@ -30,12 +31,16 @@ config_dest = cur_dir + "/" + "config.json"
 with open(config_dest, "w") as text_file:
     text_file.write(config_json)
 
+iplib = ""
+if args.iplib is not None:
+	iplib = " -i " + args.iplib
+
 RunCmd = "\
 	cd /vivado/build;\
 	git clone git@bitbucket.org:cusdr/cunoc_sdr.git;\
 	cd /vivado/build/cunoc_sdr;\
 	cp /vivado/build/config.json /vivado/build/cunoc_sdr/;\
-	python run.py gen -v " + args.vivado + " -c config.json;\
+	python run.py gen -v " + args.vivado + " -c config.json" + iplib + ";\
 	python run.py run;\
 	tar -czf /vivado/build/result.tar.gz BOOT.BIN config.json.out Build_Dir/AXIS_SWITCH_IP_gen.tcl Build_Dir/VER_CTRL_REG_IP_gen.tcl Build_Dir/Base_build.tcl Build_Reports \
 		system.bit fsbl.elf Zynq_Sys_Build/boot_bin/zynq.bif Zynq_Sys_Build/uboot/u-boot.elf Zynq_Sys_Build/linux_image/uImage Zynq_Sys_Build/device_tree/zynq-zed.dtb\
